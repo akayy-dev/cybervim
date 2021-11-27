1125dae1a840946bb7903e47a6563637fd5a56cf
Ahadu Kebede <ahadukebede@gmail.com>
2021-11-27 15:01:11 -0500

    HTML Tag pairing

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,5 +5,6 @@ require'nvim-treesitter.configs'.setup {
 		extended_mode = true,
 		max_file_lines = nil,
 		colors = {'#FFFFFF', '#FF79C6', '#008080', '#50FA7B', '#F1FA8C', '#FFB86C', '#FF5555'}
-	}
+	},
+	autotag = { enable = true }
 }
