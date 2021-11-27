c347a5db70fcf543cf9f2d5e18f78a467970ae41
Ahadu Kebede <ahadukebede@gmail.com>
2021-11-27 14:59:03 -0500

    Added html support

---
+++ lua/code/html.lua
@@ -0,0 +1,7 @@
+vim.bo.expandtab = false  -- Use tabs for indentation
+vim.opt.wrap = true       -- Line wrapping
+vim.opt.number = true
+vim.opt.autoindent = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth= 2
