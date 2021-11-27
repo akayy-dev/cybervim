c8db0efb079ff67d630899a89b7636de3d66f8ef
Ahadu Kebede <ahadukebede@gmail.com>
2021-11-27 10:21:23 -0500

    Fixed issue where some keybindings wouldn't work

--- lua/keymaps/git.lua
+++ lua/keymaps/git.lua
@@ -5,5 +5,5 @@ local opts = {noremap = true}
 keymap('n', '<leader>gs', '<cmd>Git<cr>', opts)
 keymap('n', '<leader>gS', '<cmd>Telescope git_status<cr>', opts)
 keymap('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', opts)
-keymap('n', '<leader>gf', 'Telescope git_files', opts)
-keymap('n', '<leader>gb', 'Telescope git_branches', opts)
+keymap('n', '<leader>gf', '<cmd>Telescope git_files<cr>', opts)
+keymap('n', '<leader>gb', '<cmd>Telescope git_branches<cr>', opts)
