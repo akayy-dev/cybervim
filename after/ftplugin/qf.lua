f0daeb14f7c0af37b3e3f083bb0f939ca9cb1b32
Ahadu Kebede <ahaduk@archyoga.localdomain>
2022-04-12 18:45:16 -0400

    Made q to quit quickfix a local keymap

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,7 +1,9 @@
 vim.o.relativenumber = true
 
+-- TODO: Convert vim.cmd into a lua buffer keymap when possible
 -- Keymaps
-local keymap = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+-- local keymap = vim.api.nvim_buf_set_keymap
+--local opts = {noremap = true, silent = true}
 
-keymap('n', 'q', '<cmd>cclose<CR>', opts)
+-- keymap('n', 'q', '<cmd>cclose<CR>', opts)
+vim.cmd[[nnoremap <buffer> q <cmd>cclose<CR>]]
