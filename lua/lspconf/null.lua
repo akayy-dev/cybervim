3a7f7f15d6965f09b67c546868a01520aea29331
ahoodatheguy <ahadukebede@gmail.com>
2022-04-17 08:11:12 -0400

    Configured `flake8`

--- lua/lspconf/null.lua
+++ lua/lspconf/null.lua
@@ -6,7 +6,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--ignore = W191, D103, E501" } }),
 		null_ls.builtins.formatting.autopep8,
 	},
 })
