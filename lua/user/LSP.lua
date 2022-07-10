187ed099fd4d66de292d471a53835de21a70734f
Ahadu Kebede <ahaduk@archyoga.localdomain>
2022-07-10 19:13:44 -0400

    Fixed `nlsp-settings`

--- lua/user/LSP.lua
+++ lua/user/LSP.lua
@@ -20,4 +20,8 @@ lsp_installer.on_server_ready(function(server)
 end)
 
 -- Configure server with JSON
-require'nlspsettings'.setup()
+require'nlspsettings'.setup({
+	config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
+	local_settings_dir = 'nlsp-settings',
+	loader = 'json'
+})
