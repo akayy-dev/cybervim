194337e20dbc880aa41299c5934eae9378b8cfce
Ahadu Kebede <ahaduk@archyoga.localdomain>
2022-07-11 22:08:28 -0400

    used lspsaga for hover docs.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -11,7 +11,7 @@ keymap('n', 'Q', '<cmd>copen<CR>', opts)
 
 -- LSP
 keymap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-keymap('n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap('n', 'K', "<cmd>Lspsaga hover_doc<CR>", opts)
 keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 keymap('n', 'gp', '<cmd>TroubleToggle<CR>', opts)
