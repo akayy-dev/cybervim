45a43ccc86189cc4bbc4c60d251291403fb505d2
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-03 00:09:12 -0400

    Added telescope keybinds

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,7 +4,6 @@ local opts = {noremap = true, silent = true}
 
 
 -- File Tree
-keymap('n', '<leader>p', '<cmd>NvimTreeToggle<CR>', opts)
 
 -- Quickfix
 keymap('n', 'Q', '<cmd>copen<CR>', opts)
@@ -14,7 +13,6 @@ keymap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 keymap('n', 'K', "<cmd>Lspsaga hover_doc<CR>", opts)
 keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-keymap('n', 'gp', '<cmd>TroubleToggle<CR>', opts)
 
 -- Buffer Management
 keymap('n', 'L', "<cmd>bnext<CR>", opts)
@@ -22,3 +20,10 @@ keymap('n', 'H', "<cmd>bprevious<CR>", opts)
 
 -- Search commands
 keymap('n', '<leader><leader>', '<cmd>Telescope commands theme=ivy<CR>', opts)
+
+
+-- Telescope
+keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts) -- Search Files
+keymap('n', '<leader>fg', '<cmd>Telescope live_grep theme=dropdown<CR>', opts) -- Search project for string
+keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts) -- Search for help tags
+keymap('n', '<leader>?', '<cmd>Telescope keymaps<CR>', opts) -- Search keybindings
