9aae514f46be2a3c575f778df5f6ad08eac39b51
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-04 21:55:22 -0400

    Added snippet completion

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -4,23 +4,31 @@ local has_words_before = function()
 end
 
 local config = function()
-	local cmp = require('cmp')
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
 	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
 		window = {
 			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered()
+			documentation = cmp.config.window.bordered(),
 		},
 		sources = cmp.config.sources({
-			{ name = 'nvim_lsp' },
-			{ name = 'path' },
-			{ name = 'buffer' }
+			{ name = "nvim_lsp" },
+			{ name = "path" },
+			{ name = "buffer" },
 		}),
 		mapping = cmp.mapping.preset.insert({
-			['<CR>'] = cmp.mapping.confirm({ select = true }),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
 			-- Tab & Shift TabCompletion
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
 				elseif has_words_before() then
 					cmp.complete()
 				else
@@ -28,43 +36,41 @@ local config = function()
 				end
 			end, { "i", "s" }),
 			["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 		}),
-		})
-
-	-- Use cmp as an autocomplete source for searching the buffer with / and ?
-	cmp.setup.cmdline({'/', '?'}, {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources(
-			{
-				{ name = 'buffer' }
-			}
-		)
 	})
 
-	cmp.setup.cmdline({':'}, {
+	-- Use cmp as an autocomplete source for searching the buffer with / and ?
+	cmp.setup.cmdline({ "/", "?" }, {
 		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources(
-			{
-				{ name = 'cmdline' }
-			}
-		)
+		sources = cmp.config.sources({
+			{ name = "buffer" },
+		}),
+	})
+
+	cmp.setup.cmdline({ ":" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "cmdline" },
+		}),
 	})
 end
 
-
 return {
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/nvim-cmp",
 	dependencies = {
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-cmdline'
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
 	},
-	config = config
+	config = config,
 }
