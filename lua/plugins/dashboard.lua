e1dc973d283a05a7740e0804d2615be16e0d5b55
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-05 14:33:51 -0400

    Customized dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,13 +1,66 @@
+local header = {
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+"            __                    ",
+ "    ____ _/ /______ ___  ____  __",
+"   / __ `/ //_/ __ `/ / / / / / /",
+" / /_/ / ,< / /_/ / /_/ / /_/ /" ,
+" \\__,_/_/|_|\\__,_/\\__, /\\__, /"  ,
+"                /____//____/",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+			}
+
+
 local config = {
-	theme='hyper',
-	config = {
-		project = {
-			enable = true,
-			label = 'Projects',
-			limit = 8,
-			action = function(path)
-				vim.cmd('Telescope find_files cwd=' .. path)
-		end}
+theme='doom',
+config = {
+	header = header,
+	center = {
+		{
+			icon = ' ',
+			icon_hl = 'group',
+			desc = 'Projects           ',
+			desc_hl = 'group',
+			key = 'fp',
+			key_hl = 'group',
+			key_format = ' [%s]',
+			action = 'Telescope projects theme=ivy'
+		},
+		{
+			icon = ' ',
+			icon_hl = 'group',
+			desc = 'New File           ',
+			desc_hl = 'group',
+			key = 'n',
+			key_hl = 'group',
+			key_format = ' [%s]',
+			action = 'enew'
+		},
+		{
+			icon = ' ',
+			icon_hl = 'group',
+			desc = 'Recent Files           ',
+			desc_hl = 'group',
+			key = 'fr',
+			key_hl = 'group',
+				key_format = ' [%s]',
+				action = 'Telescope oldfiles theme=ivy'
+			},
+
+		},
 	}
 }
 
