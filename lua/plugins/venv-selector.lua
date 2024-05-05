95b638dc805e2e73b03542f5c991bfd0fe11fb02
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-05 16:53:43 -0400

    Added python virtualenvironment selector

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,14 @@
+return {
+	'linux-cultist/venv-selector.nvim',
+	dependencies = {
+			'neovim/nvim-lspconfig',
+			'nvim-telescope/telescope.nvim',
+			'mfussenegger/nvim-dap-python',
+	},
+	config = function ()
+		require('venv-selector').setup {
+			name = { "env", "venv", ".venv" },
+			path = './'
+		}
+	end
+}
