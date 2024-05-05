68031d2f03aedd3e4fa8c332c89af1335da169ea
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-05 17:51:44 -0400

    Showing hidden items in telescope and neo-tree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,14 @@ return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = {
 		'nvim-lua/plenary.nvim'
-	}
+	},
+	config = function ()
+		require('telescope').setup{
+			pickers = {
+				find_files = {
+					hidden = true
+				}
+			}
+		}
+	end
 }
