69c8a644ef031cc9b695f867c7d7faafc88ca8e2
Ahadu Kebede <ahadukebede@gmail.com>
2024-05-06 18:43:00 -0400

    More config stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,47 +4,48 @@ return {
 		opts = {},
 		config = {
 			function()
-				require('mason').setup({
-					ensure_installed = {'lua_ls', 'pyright'}
+				require("mason").setup({
+					ensure_installed = { "lua_ls", "pyright" },
 				})
-			end
-		}
+			end,
+		},
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls" }
+			ensure_installed = { "lua_ls" },
 		},
 	},
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		config = function()
-				local mason_config = require('mason-lspconfig')
-				mason_config.setup()
-				mason_config.setup_handlers{
-					function (server_name)
-						require("lspconfig")[server_name].setup{}
-					end,
-					['lua_ls'] = function()
-						local lspconfig = require('lspconfig')
-						lspconfig.lua_ls.setup{
+			require("lsp-zero").preset({ manage_nvim_cmp = false })
+			local mason_config = require("mason-lspconfig")
+			mason_config.setup()
+			mason_config.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
 						settings = {
 							Lua = {
 								diagnostics = {
-									globals = { "vim", "hs" }
-								}
-							}
-						}
-					}
-					end
-				}
-		end
+									globals = { "vim", "hs" },
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require('lspconfig')
-		end
+			local lspconfig = require("lspconfig")
+		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,12 @@ return {
 	},
 	config = function ()
 		require('telescope').setup{
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					".git/"
+				}
+			},
 			pickers = {
 				find_files = {
 					hidden = true
